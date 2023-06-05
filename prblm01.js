573c7d6becdf3f81ecab5ee673425a06a782217a
Aman Rawat <94544552+i-AmanRawat@users.noreply.github.com>
2023-06-05 17:09:53 +0530

    Update prblm01.js with 2nd solution
    adding another method to solve the same problem

--- prblm01.js
+++ prblm01.js
@@ -1,5 +1,7 @@
 // question: You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+// i have writen 2 solution: 1st solution is my first approach and 2nd solution i have written after reading sol of chatgpt for the same problem.
 
+//METHOD 01
 coordinates = [
   [1, 2],
   [2, 3],
@@ -48,3 +50,35 @@ function straightLineChecker(slopes) {
 straightLineChecker(slopeCalculator(coordinates))
   ? console.log("It is a straight line")
   : console.log("It is not a straight line");
+
+//METHOD 02
+function straightLineChecker(coordinates) {
+  if (coordinates.length === 2) {
+    //since there are 2 coordinates given it's obvious it is a st line
+    return true;
+  }
+  let slopes = [];
+  for (let i = 1; i < coordinates.length; i++) {
+    let firstPoint = coordinates[i - 1];
+    let secondPoint = coordinates[i];
+    x1 = firstPoint[0];
+    y1 = firstPoint[1];
+    x2 = secondPoint[0];
+    y2 = secondPoint[1];
+    const initialSlope =
+      coordinates[1][1] -
+      coordinates[0][1] / coordinates[1][0] -
+      coordinates[0][0];
+    let slope = (y2 - y1) / (x2 - x1);
+    if (slope !== initialSlope) {
+      //slope is not equals to initial slope
+      return false;
+    }
+  }
+  return true;
+}
+
+straightLineChecker(coordinates)
+  ? console.log("Straight Line")
+  : console.log("Not a Straight Line");
+
